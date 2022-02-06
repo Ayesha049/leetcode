276c48b911c3be773ebb8eb64d17038819f2af23
Ayesha Siddiqua <1405049as@gmail.com>
2022-02-06 12:49:07 +0600

    Time: 692 ms (6.89%), Space: 121.6 MB (5.01%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.java
+++ 1711-count-good-meals/1711-count-good-meals.java
@@ -3,13 +3,19 @@ class Solution {
         long out=0;
         long mod=1000000007;
         HashMap<Integer,Integer>map=new HashMap<Integer,Integer>();
+        List<Integer> list = new ArrayList();
+        int base = 1;
+        list.add(base);
+        for(int i=0;i<=20;i++) {
+            base*=2;
+            list.add(base);
+        }
         for(int num:deliciousness){
-            for(int i=0;i<31;i++){
-                int powerof2= 1<<i;
-                int req=powerof2-num;
+            for(int pow:list){
+                int req=pow-num;
                 if(map.containsKey(req)){
                    out+=map.get(req); 
-                    out%=mod;
+                   out%=mod;
                 }
             }
             map.put(num,map.getOrDefault(num,0)+1);
