6f1b00361611c448222f64a68ee86e209ea8fbee
Ayesha Siddiqua <1405049as@gmail.com>
2022-03-24 11:39:01 +0600

    Time: 11 ms (14.63%), Space: 6.8 MB (20.10%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    vector<int> partitionLabels(string S) {
-        vector<int>ans;
-        if(S.length() == 0) return ans;
-        unordered_map<char,int>fst,lst;
-        for(int i=0;i<S.length();i++) {
-            if(fst.find(S[i])==fst.end()) {
-                fst[S[i]] = i;
-                lst[S[i]] = i;
-            } else lst[S[i]] = i;
+    vector<int> partitionLabels(string s) {
+        vector<int> ans;
+        map<int,int>endPos;
+        for(int i = 0; i < s.length(); i++) {
+            endPos[s[i]] = i;
         }
-        int f = fst[S[0]];
-        int l = lst[S[0]];
-        for(int i = 1; i < S.length(); i++) {
-            if(fst[S[i]] > l) {
-                ans.push_back(l-f+1);
-                f = fst[S[i]];
-                l = lst[S[i]];
-            } else {
-                l = max(lst[S[i]],l);
+        
+        int st = 0;
+        int l = endPos[s[st]];
+        int i=0;
+        while(i<s.length()) {
+            // cout<<s[i]<<" "<<endPos[s[i]]<<endl;
+            while(i<=l) {
+                int curEnd =  endPos[s[i]];
+                i++;
+                l = max(l,curEnd);
             }
+            ans.push_back((l-st) + 1);
+            st=i;
+            l = endPos[s[i]];
+            
         }
-        ans.push_back(l-f+1);
         return ans;
     }
 };
