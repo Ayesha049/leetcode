87954322b564dbdc16b486c0a8cc37048bb5cbc5
Ayesha Siddiqua <1405049as@gmail.com>
2022-03-25 23:20:02 +0600

    Time: 268 ms (11.67%), Space: 111.5 MB (73.05%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,41 +1,65 @@
 class Solution {
 public:
-    int replaceCount (vector<int>& A, vector<int>& B, int val) {
-        int ans = 0;
-        for (int i = 0; i < A.size(); i++) {
-            if (A[i] == val) continue;
-            if (B[i] == val) {
-                ans++;
-                continue;
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+        int top = 0;
+        int bottom = 0;
+        int swapTop = 1;
+        int swapBottom = 1;
+        //top calculation
+        int n = tops.size();
+        int val = tops[0];
+        for(int i=1;i<n;i++) {
+            if(val != tops[i]) {
+                if(val != bottoms[i]) {
+                    top = -1;
+                    break;
+                } else {
+                    top++;
+                }
             }
-            return -1;
-        }
-        return ans;
-    }
-    int minDominoRotations(vector<int>& A, vector<int>& B) {
-        int ans = -1;
-        int count = replaceCount(A,B,A[0]);
-        if (count!=-1) ans = count;
-        
-        count = replaceCount(A,B,B[0]);
-        if (count!=-1) {
-            if(ans == -1) ans = count;
-            else ans = min(ans,count);
         }
         
-        count = replaceCount(B,A,A[0]);
-        if (count!=-1) {
-            if(ans == -1) ans = count;
-            else ans = min(ans,count);
+        //swap top calculation
+        val = bottoms[0];
+        for(int i=1;i<n;i++) {
+            if(val != tops[i]) {
+                if(val != bottoms[i]) {
+                    swapTop = -1;
+                    break;
+                } else {
+                    swapTop++;
+                }
+            }
+        }
+        val = bottoms[0];
+        //bottom
+        for(int i=1;i<n;i++) {
+            if(val != bottoms[i]) {
+                if(val != tops[i]) {
+                    bottom = -1;
+                    break;
+                } else {
+                    bottom++;
+                }
+            }
         }
         
-        count = replaceCount(B,A,B[0]);
-        if (count!=-1) {
-            if(ans == -1) ans = count;
-            else ans = min(ans,count);
+        //swapBottom
+        val = tops[0];
+        for(int i=1;i<n;i++) {
+            if(val != bottoms[i]) {
+                if(val != tops[i]) {
+                    swapBottom = -1;
+                    break;
+                } else {
+                    swapBottom++;
+                }
+            }
         }
-        
-        return ans;
-        
+        int bestTop = top == -1 && swapTop == -1 ? -1 : min(top == -1 ? INT_MAX : top, swapTop == -1 ? INT_MAX : swapTop);
+        int bestBottom = bottom == -1 && swapBottom == -1 ? -1 : min(bottom == -1 ? INT_MAX : bottom, swapBottom == -1 ? INT_MAX : swapBottom);
+        if(bestTop == -1) return bestBottom;
+        if(bestBottom == -1) return bestTop;
+        return min(bestTop,bestBottom);
     }
 };
