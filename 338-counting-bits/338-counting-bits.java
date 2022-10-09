0e1ba5192b106458c0a9d7b7b00107069a316eec
Ayesha Siddiqua <1405049as@gmail.com>
2022-10-09 12:58:33 +0600

    Time: 4 ms (49.60%), Space: 48.1 MB (56.33%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -12,15 +12,16 @@ class Solution {
         if(n == 0) return 0;
         if(n == 1) return 1;
         
-        if(memo[n] != 0) return memo[n]; // if memo of n answer is already available we will re-use it & not go further for calculation
-        // but if you are coming for the first time then, store that value in memo
-        if(n % 2 == 0) {
-            memo[n] = solve(n / 2, memo);
-            return solve(n / 2, memo);
+        if(memo[n] == 0) { 
+            if(n % 2 == 0) {
+                memo[n] = solve(n / 2, memo);
+
+            }
+            else {
+                memo[n] = 1 + solve(n / 2, memo);
+            } 
         }
-        else {
-            memo[n] = 1 + solve(n / 2, memo);
-            return 1 + solve(n / 2, memo);
-        } 
+        
+        return memo[n];
     }
 }
